b00989ed2b0c761564946500e282f162797decce
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-04-03 15:19:48 -0300

    :bricks: Atualizando condições para inserção de operador

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,13 +43,16 @@ teclas.addEventListener("click", (e) => {
     const displayedNum = display.textContent;
     let resultado = '';
     let operadorAnterior = '';
+    let valorAnterior = '';
+    const oepradores = (/[-/*+]/g);
+    const numeros = (/['0123456789']/g);
 
     if (!action) {
       if (displayedNum === "0" || resultado !== '') {
         display.textContent = teclaConteudo;
         resultado = '';
       } else {
-        display.textContent = displayedNum + teclaConteudo;
+        display.textContent = displayedNum + teclaConteudo
       }
         operadorAnterior = '';
     }
@@ -61,10 +64,16 @@ teclas.addEventListener("click", (e) => {
     }
     if (action === "add" ||
         action === "sub") {
-          display.textContent = displayedNum + teclaConteudo;
-          operadorAnterior = teclaConteudo;
-          console.log(operadorAnterior);
-        }
+          valorAnterior = displayedNum;
+          console.log(valorAnterior);
+          console.log(numeros.test(valorAnterior.length(0, -1)));
+          console.log(numeros.test(valorAnterior.length(0, -1)));
+          }
+          // display.textContent = displayedNum + teclaConteudo;
+          // operadorAnterior = teclaConteudo;
+          // console.log(operadorAnterior);
+          // console.log(reg.test(operadorAnterior));
+        
     if (action === "mult") {
       display.textContent = displayedNum + '*';
       operadorAnterior = '*';
